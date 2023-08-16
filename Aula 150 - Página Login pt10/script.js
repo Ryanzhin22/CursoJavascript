062f87c315127352bd10d2822e7a20b5b5ad392d
Ryan <103447125+Ryanzhin22@users.noreply.github.com>
2023-08-16 16:01:20 -0300

    Add files via upload

---
+++ Aula 150 - Página Login pt10/script.js
@@ -0,0 +1,16 @@
+import { Cxmsg } from "../Aula 139 - Mensagem Personalizada pt5/cxmsg.js"
+
+const callback_ok = () => {}
+
+const callback_noOk = () => {
+  const config = {
+    cor: "#800",
+    tipo: "ok",
+    textos: null,
+    comando_sn: null,
+  };
+  Cxmsg.mostrar(config, "Erro", "Login não efetuado! Usuário ou senha incorretos.");
+}
+
+
+Login.login(callback_ok, callback_noOk);
